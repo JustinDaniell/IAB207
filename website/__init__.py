4e9cf809ae20421ee03839540cbf2df2aea74741
glenntantt <glenntantt@gmail.com>
2024-10-22 17:41:06 +1000

    travel-app

--- website/__init__.py
+++ website/__init__.py
@@ -1,45 +1,45 @@
-# import flask - from 'package' import 'Class'
-from flask import Flask 
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+import datetime
 
 db = SQLAlchemy()
 
-# create a function that creates a web application
-# a web server will run this web application
 def create_app():
-  
-    app = Flask(__name__)  # this is the name of the module/package that is calling this app
-    # Should be set to false in a production environment
-    app.debug = True
-    app.secret_key = 'somesecretkey'
-    # set the app configuration data 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitedata.sqlite'
-    # initialise db with flask app
-    db.init_app(app)
+    app = Flask(__name__)
 
-    Bootstrap(app)
-    
-    # initialise the login manager
     login_manager = LoginManager()
-    
-    # set the name of the login function that lets user login
-    # in our case it is auth.login (blueprintname.viewfunction name)
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view='auth.login'
     login_manager.init_app(app)
 
-    # create a user loader function takes userid and returns User
-    # Importing inside the create_app function avoids circular references
-    from .models import User
+
+    from .models import User  # importing here to avoid circular references
     @login_manager.user_loader
     def load_user(user_id):
-       return db.session.scalar(db.select(User).where(User.id==user_id))
+        return User.query.get(int(user_id))
 
-    from . import views
-    app.register_blueprint(views.main_bp)
+    # we use this utility module to display forms quickly
+    Bootstrap(app)
 
-    from . import auth
-    app.register_blueprint(auth.auth_bp)
+    # A secret key for the session object
+    app.secret_key = 'somerandomvalue'
+
+    # Configue and initialise DB
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///traveldb.sqlite'
+    db.init_app(app)
+
+    # config upload folder
+    UPLOAD_FOLDER = '/static/image'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     
+    # add Blueprints
+    from . import views
+    app.register_blueprint(views.mainbp)
+    from . import destinations
+    app.register_blueprint(destinations.destbp)
+    from . import auth
+    app.register_blueprint(auth.authbp)
+
     return app
