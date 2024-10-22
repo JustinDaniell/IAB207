a08e6899f4365963357e90abe682e2c44a960989
glenntantt <glenntantt@gmail.com>
2024-10-22 17:17:11 +1000

    original

--- website/__init__.py
+++ website/__init__.py
@@ -1,62 +1,45 @@
-
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+# import flask - from 'package' import 'Class'
+from flask import Flask 
+from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
-import datetime
 
 db = SQLAlchemy()
 
+# create a function that creates a web application
+# a web server will run this web application
 def create_app():
-    app = Flask(__name__)
-    #we use this utility module to display forms quickly
-    Bootstrap(app)
-
-    #this is a much safer way to store passwords
-    Bcrypt(app)
-
-    #a secret key for the session object
-    #(it would be better to use an environment variable here)
-    app.secret_key = 'somerandomvalue'
-
-    #Configue and initialise DB
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///traveldb.sqlite'
+  
+    app = Flask(__name__)  # this is the name of the module/package that is calling this app
+    # Should be set to false in a production environment
+    app.debug = True
+    app.secret_key = 'somesecretkey'
+    # set the app configuration data 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitedata.sqlite'
+    # initialise db with flask app
     db.init_app(app)
 
-    #config upload folder
-    UPLOAD_FOLDER = '/static/image'
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+    Bootstrap5(app)
     
     # initialise the login manager
     login_manager = LoginManager()
+    
+    # set the name of the login function that lets user login
+    # in our case it is auth.login (blueprintname.viewfunction name)
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
     # create a user loader function takes userid and returns User
-    from .models import User  # importing here to avoid circular references
+    # Importing inside the create_app function avoids circular references
+    from .models import User
     @login_manager.user_loader
     def load_user(user_id):
-        return db.session.scalar(db.select(User).where(User.id==user_id))
+       return db.session.scalar(db.select(User).where(User.id==user_id))
 
-    # add Blueprints
     from . import views
-    app.register_blueprint(views.mainbp)
-    from . import destinations
-    app.register_blueprint(destinations.destbp)
+    app.register_blueprint(views.main_bp)
+
     from . import auth
-    app.register_blueprint(auth.authbp)
-
-    @app.errorhandler(404) 
-    # inbuilt function which takes error as parameter 
-    def not_found(e): 
-      return render_template("404.html", error=e)
-
-    # this creates a dictionary of variables that are available
-    # to all html templates
-    @app.context_processor
-    def get_context():
-      year = datetime.datetime.today().year
-      return dict(year=year)
-
+    app.register_blueprint(auth.auth_bp)
+    
     return app
--- website/views.py
+++ website/views.py
@@ -1,20 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from .models import Destination
-from . import db
+from flask import Blueprint
 
-mainbp = Blueprint('main', __name__)
+main_bp = Blueprint('main', __name__)
 
-@mainbp.route('/')
+@main_bp.route('/')
 def index():
-    destinations = db.session.scalars(db.select(Destination)).all()    
-    return render_template('index.html', destinations=destinations)
-
-@mainbp.route('/search')
-def search():
-    if request.args['search'] and request.args['search'] != "":
-        print(request.args['search'])
-        query = "%" + request.args['search'] + "%"
-        destinations = db.session.scalars(db.select(Destination).where(Destination.description.like(query)))
-        return render_template('index.html', destinations=destinations)
-    else:
-        return redirect(url_for('main.index'))
+    return '<h1>Starter code for assignment 3<h1>'
